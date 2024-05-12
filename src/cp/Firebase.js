65d2fbcfcc3480af5a13ef8eab2949d3fb82e5f2
varunseth578 <ivarunseth@gmail.com>
2024-05-12 12:53:36 +0530

    deploy on firebase

--- src/cp/Firebase.js
+++ src/cp/Firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC1w27OY6DwUEeaytTk5qCNrLWNDiATdHM",
-  authDomain: "fir-netflix-gpt0.firebaseapp.com",
-  projectId: "fir-netflix-gpt0",
-  storageBucket: "fir-netflix-gpt0.appspot.com",
-  messagingSenderId: "710142278397",
-  appId: "1:710142278397:web:818345280d22d9a38d9c94",
-  measurementId: "G-B9PVLBL984"
+  apiKey: "AIzaSyC2jHcexA6HnE4YWu5PwBpWg5Uy59wAngQ",
+  authDomain: "fir-demo-netflix-gpt0.firebaseapp.com",
+  projectId: "fir-demo-netflix-gpt0",
+  storageBucket: "fir-demo-netflix-gpt0.appspot.com",
+  messagingSenderId: "807056924127",
+  appId: "1:807056924127:web:9dab6aaeda23df253ab86a",
+  measurementId: "G-5T6BWMH2RX"
 };
 
 // Initialize Firebase
