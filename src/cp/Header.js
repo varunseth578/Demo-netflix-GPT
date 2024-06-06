9dd7deb94c2e671588d6675cf65073c895113211
varunseth578 <ivarunseth@gmail.com>
2024-06-06 21:17:36 +0530

    minor upgrade

--- src/cp/Header.js
+++ src/cp/Header.js
@@ -1,15 +1,16 @@
 import React, { useEffect } from "react";
 import { addUser, removeUser } from "../utils/userSlice";
-import { onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged ,signOut } from "firebase/auth";
 import { auth } from "../utils/Firebase";
-import { useDispatch } from "react-redux";
+import { useDispatch ,useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { LOGO, USER } from "../utils/constants";
-import { signOut } from "firebase/auth";
+
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
 
   const handleSignOut = () => {
     signOut(auth)
@@ -35,17 +36,17 @@ const Header = () => {
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between no-scrollbar">
       <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="logo" />
+      {user && (<div className="flex p-2 justify-between">
+      <button className=" text-red-600 font-bold text-2xl p-4 px-12 rounded-lg hover:text-red-500 ">Home</button>
+      <button className=" text-red-600 font-bold text-2xl p-4 px-12 rounded-lg hover:text-red-500  ">Movies</button>
+      <button className=" text-red-600 font-bold text-2xl p-4 px-12 rounded-lg hover:text-red-500  ">TV Shows</button>
+        <img className=" hidden w-12 h-12" alt="usericon" src={USER} />
 
-      <div className="flex p-2 justify-between">
-      <button className=" text-red-600 font-bold text-2xl p-4 px-12 rounded-lg ">Home</button>
-      <button className=" text-red-600 font-bold text-2xl p-4 px-12 rounded-lg ">Movies</button>
-      <button className=" text-red-600 font-bold text-2xl p-4 px-12 rounded-lg ">TV Show</button>
-        <img className=" w-12 h-12" alt="usericon" src={USER} />
-
-        <button onClick={handleSignOut} className="font-bold text-xl text-red-600 ">
-          (Sign Out)
+        <button onClick={handleSignOut} className="font-bold text-2xl text-red-600 hover:text-red-500  ">
+          Sign Out
         </button>
-      </div>
+      </div> )}
+      
     </div>
   );
 };
