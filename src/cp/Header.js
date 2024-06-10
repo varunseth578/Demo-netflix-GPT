96de12c78ac06334c263891065160547b483f17b
varunseth578 <ivarunseth@gmail.com>
2024-06-10 14:40:47 +0530

    minor upgrade

--- src/cp/Header.js
+++ src/cp/Header.js
@@ -12,11 +12,12 @@ const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
-  const showGptSearch = useSelector((store)=> store.gpt.showGptSearch)
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
 
   const handleGpt = () => {
     dispatch(toggleGptSearch());
   };
+
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {})
@@ -38,36 +39,35 @@ const Header = () => {
     });
     return () => unsubscribe();
   }, []);
-  const handlelang= (e) => {
+  const handlelang = (e) => {
     dispatch(changeLang(e.target.value));
-   
   };
-  
+
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between no-scrollbar">
       <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="logo" />
       {user && (
         <div className="flex p-2 ">
-        {showGptSearch && (<select className="p-2 m-2 bg-black text-white rounded-lg" onChange={handlelang}>
-            {SUPPORTED_LANG.map((lang) => (
-              <option key={lang.identifier} value={lang.identifier}>
-                {lang.name}
-              </option>
-            ))}
-          </select>)}
-          
+          {showGptSearch && (
+            <select
+              className="p-2 m-2 bg-black text-white rounded-lg"
+              onChange={handlelang}
+            >
+              {SUPPORTED_LANG.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
+
           <button
             onClick={handleGpt}
             className=" text-red-600 font-bold text-2xl p-4 px-12 rounded-lg hover:text-red-500 "
           >
             {showGptSearch ? "Home" : "GPT Search"}
           </button>
-          <button className=" text-red-600 font-bold text-2xl p-4 px-12 rounded-lg hover:text-red-500  ">
-            Movies
-          </button>
-          <button className=" text-red-600 font-bold text-2xl p-4 px-12 rounded-lg hover:text-red-500  ">
-            TV Shows
-          </button>
+
           <img className=" hidden w-12 h-12" alt="usericon" src={USER} />
 
           <button
